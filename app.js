49d403dcc958bcda5bff890fc7044610cf5a9006
AirLinkDev <thomas.trebacz@gmail.com>
2023-11-11 13:28:48 -0800

    Authentication implemented through passport passport-local-mongoose

--- app.js
+++ app.js
@@ -3,33 +3,49 @@ import express from "express";
 import bodyParser from "body-parser";
 //import ejs from "ejs";
 import { mongoose } from "mongoose";
-import encrypt from "mongoose-encryption";
-import dotenv from "dotenv";
+//import encrypt from "mongoose-encryption";
+//import dotenv from "dotenv";
 //import md5 from "md5";
-import bcrypt from "bcrypt";
-const saltRounds = 10;
-dotenv.config();
-const uri = "mongodb://127.0.0.1:27017/userDB";
-//mongoose.connect("mongodb://localhost:27017/userDB");
-//IMPORTANT: mongoose.connect does not work with "localhost"
-mongoose.connect(uri)
-.then(()=>console.log("DB Connected"))
-.catch((err)=>console.log(err));
+//import bcrypt from "bcrypt";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
 
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-});
-userSchema.plugin(encrypt,{secret : process.env.SECRET,encryptedFields : ["password"] });
-//add additional fields into array as needed
-const User = new mongoose.model("user",userSchema);
-
+//const saltRounds = 10;
+//dotenv.config();
 const app = express();
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+const uri = "mongodb://127.0.0.1:27017/userDB";
+//mongoose.connect("mongodb://localhost:27017/userDB");
+//IMPORTANT: mongoose.connect does not work with "localhost"
+mongoose.connect(uri)
+.then(()=>console.log("DB Connected"))
+.catch((err)=>console.log(err));
+//mongoose.set("useCreateIndex",true);
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+//userSchema.plugin(encrypt,{secret : process.env.SECRET,encryptedFields : ["password"] });
+userSchema.plugin(passportLocalMongoose);
+//add additional fields into array as needed
+const User = new mongoose.model("user",userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 
@@ -42,6 +58,7 @@ app.get("/login", function(req,res){
 
     }); 
 app.get("/logout", function(req,res){
+    req.logout();
     res.render("home");
 
     }); 
@@ -49,68 +66,41 @@ app.get("/logout", function(req,res){
 app.get("/register", function(req,res){
     res.render("register");
 });
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
 app.post("/login", async function(req,res){
-    const username= req.body.username;
-    try{
-        /***
-         * Capturing data from db is impossible without async and await
-         */
-    const foundUser = await User.findOne({email: req.body.username});
-
-    console.log("db returned user: "+foundUser);
-        //bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
-        // result == false
-    //});
-    bcrypt.compare(req.body.password,foundUser.password, function(err,result){
-        console.log("Inside Compare Method");
-        if(result){
-            console.log("password matched!!");
-            res.render("secrets");
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
         }else{
-            console.log("password did not match!!");
-            res.render("login");
+            passport.authenticate("local")(req,res, function(){
+                res.render("secrets");
+            });              
         }
     })
-        
-       
-
-
-
-    }catch(err){
-        if(err){
-            console.log("findOne got Error: " + err);
-        }else{
-            console.log("password NOT matched!!");
-            res.render("login");
-        }
-    }
-
-
-
-    
-    
-
 });
 app.post("/register", async function(req,res){
-    const newUser = await bcrypt.hash(req.body.password,saltRounds,function(err,hash){
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        try{
-            newUser.save();
-        }catch(err){
-            if(err){
-                console.log("we got Error: " + err);
-            }
-        }
-        console.log("Trying to save new user: "+JSON.stringify(newUser));
-    });
-
-    
-    res.render("secrets");
-
+  User.register({username: req.body.username},req.body.password,function(err,user){
+    if(err){
+        console.log(err);
+        res.render("/register");
+    }else{
+        passport.authenticate("local")(req,res, function(){
+            res.redirect("/secrets");
+        })
+    }
+  })
 });
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
